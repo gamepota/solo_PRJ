81694647156e147a049f5858e3387f4b2a63fbb7
Dongwon <kdw686968@gmail.com>
2024-05-07 12:44:54 +0900

    first commit 설정완료

---
+++ src/main/java/com/solo_prj/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.solo_prj;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SoloPrjApplication.class);
+    }
+
+}
---
+++ src/main/java/com/solo_prj/SoloPrjApplication.java
@@ -0,0 +1,13 @@
+package com.solo_prj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloPrjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoloPrjApplication.class, args);
+    }
+
+}
